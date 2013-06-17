538675034c4f5f97b7e4a75dbd27eed3383eb13c
Ruud Denivel <ruud@devolution.be>
2013-06-17 13:57:12 +0200

    register twig service provider from STABServiceProvider

--- Provider/STABServiceProvider.php
+++ Provider/STABServiceProvider.php
@@ -12,6 +12,16 @@ class STABServiceProvider implements ServiceProviderInterface
         $app->register(new RoutingServiceProvider());
         $app->register(new DIServiceProvider());
 
+        $app->register(new Silex\Provider\TwigServiceProvider(), array(
+            'twig.path' => array(__DIR__ . '/../Resources/views/')
+        ));
+
+        $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+            $twig->addExtension(new \Nelmio\ApiDocBundle\Twig\Extension\MarkdownExtension());
+
+            return $twig;
+        }));
+
         $app['container']->get('stab.annotation.loader')->registerAnnotations();
     }
 
